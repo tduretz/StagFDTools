780629de1d4f1a9772480c9e9333ee25ad11d1b6
tduretz <duretz@uni-frankfurt.de>
2024-12-14 17:19:21 +0100

    made file standalone for the time being

--- examples/ExampleStokes.jl
+++ examples/ExampleStokes.jl
@@ -1,5 +1,20 @@
 using StagFDTools, ExtendableSparse, StaticArrays
 
+Base.@kwdef mutable struct StokesPattern
+    num     ::Union{Matrix{Int64},   Missing} = missing
+    type    ::Union{Matrix{Symbol},  Missing} = missing
+    patternVx ::Union{SMatrix,  Missing} = missing  # these could be contained in same object - but they have different sizes
+    patternVy ::Union{SMatrix,  Missing} = missing  # these could be contained in same object - but they have different sizes
+    patternPt ::Union{SMatrix,  Missing} = missing  # these could be contained in same object - but they have different sizes
+    # ideally we would like other fields also part of it (fluid pressure, microrotation)
+end
+
+Base.@kwdef mutable struct NumberingStokes
+    Vx ::Union{StokesPattern, Missing} = missing
+    Vy ::Union{StokesPattern, Missing} = missing
+    Pt ::Union{StokesPattern, Missing} = missing
+end
+
 function NumberingStokes!(N, nc)
     
     ndof  = 0
@@ -239,6 +254,7 @@ let
 
     @info "Assembly"
     M  = SparsityPatternStokes(numbering, nc)
+    display(M)
 
     # Blocks
     nV = maximum(numbering.Vy.num)
