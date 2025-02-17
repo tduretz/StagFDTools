00978c68e98da6e58b65ad2216979448ab73ffbc
Thibault Duretz <48455309+tduretz@users.noreply.github.com>
2025-02-17 14:25:01 +0100

    Update src/Utils.jl
    Co-authored-by: Albert de Montserrat <58044444+albert-de-montserrat@users.noreply.github.com>

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,2 +1,2 @@
 printxy(x) = display( rotr90(x[end:-1:1,end:-1:1]) )
-av2D(x) = @. 0.25*(x[1:end-1,1:end-1] + x[2:end-0,1:end-1,] + x[1:end-1,2:end-0] + x[2:end-0,2:end-0])
+av2D(x) = @views @. 0.25*(x[1:end-1,1:end-1] + x[2:end-0,1:end-1,] + x[1:end-1,2:end-0] + x[2:end-0,2:end-0])
