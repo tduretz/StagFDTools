0e87db0706849b0b5425a1fa89bf355f624e8946
Anna <173553840+aep4cw@users.noreply.github.com>
2025-04-23 16:35:27 +0200

    add Crank-Nicolson method to convergence analysis

--- examples/_HeatDiffusion/ExampleADHeatDiffusion_ConvergenceTest.jl
+++ examples/_HeatDiffusion/ExampleADHeatDiffusion_ConvergenceTest.jl
@@ -23,7 +23,7 @@ using ForwardDiff, Enzyme  # AD backends you want to use
 #      |     uS    |
 #      |           |
 
-function Diffusion2D(u_loc, k, s, type_loc, bcv_loc, Δ, u0, ρ, cp)
+function Diffusion2D_backwardEuler(u_loc, k, s, type_loc, bcv_loc, Δ, u0, ρ, cp)
 
     # u_loc is 3*3 matrix containing the current values of u for the whole stencil
     #             0   uN  0
@@ -81,8 +81,78 @@ function Diffusion2D(u_loc, k, s, type_loc, bcv_loc, Δ, u0, ρ, cp)
     return -(-(qxE - qxW)/Δ.x - (qyN - qyS)/Δ.y + s - ρ*cp*(uC - u0)/Δ.t)  
 end
 
+function Diffusion2D_CrankNicolson(u_loc, u0_loc, k, s, type_loc, bcv_loc, bcv0_loc, Δ, ρ, cp, θ)
+
+    uC = u_loc[2,2]
+    u0C = u0_loc[2,2]
+
+    # Boundary conditions
+    # West
+    if type_loc[1,2] === :Dirichlet
+        uW = 2*bcv_loc[1,2] - u_loc[2,2]
+        u0W = 2*bcv0_loc[1,2] - u0_loc[2,2]
+    elseif type_loc[1,2] === :Neumann
+        uW = Δ.x*bcv_loc[1,2] + u_loc[2,2]
+        u0W = Δ.x*bcv0_loc[1,2] + u0_loc[2,2]
+    elseif type_loc[1,2] === :periodic || type_loc[1,2] === :in
+        uW = u_loc[1,2] 
+        u0W = u0_loc[1,2]
+    end
+
+    # East
+    if type_loc[3,2] === :Dirichlet
+        uE = 2*bcv_loc[3,2] - u_loc[2,2]
+        u0E = 2*bcv0_loc[3,2] - u0_loc[2,2]
+    elseif type_loc[3,2] === :Neumann
+        uE = -Δ.x*bcv_loc[3,2] + u_loc[2,2]
+        u0E = -Δ.x*bcv0_loc[3,2] + u0_loc[2,2]
+    elseif type_loc[3,2] === :periodic || type_loc[3,2] === :in
+        uE = u_loc[3,2] 
+        u0E = u0_loc[3,2]
+    end
+
+    # South
+    if type_loc[2,1] === :Dirichlet
+        uS = 2*bcv_loc[2,1] - u_loc[2,2]
+        u0S = 2*bcv0_loc[2,1] - u0_loc[2,2]
+    elseif type_loc[2,1] === :Neumann
+        uS = Δ.y*bcv_loc[2,1] + u_loc[2,2]
+        u0S = Δ.y*bcv0_loc[2,1] + u0_loc[2,2]
+    elseif type_loc[2,1] === :periodic || type_loc[2,1] === :in
+        uS = u_loc[2,1]
+        u0S = u0_loc[2,1]
+    end
+
+    # North
+    if type_loc[2,3] === :Dirichlet
+        uN = 2*bcv_loc[2,3] - u_loc[2,2]
+        u0N = 2*bcv0_loc[2,3] - u0_loc[2,2]
+    elseif type_loc[2,3] === :Neumann
+        uN = -Δ.y*bcv_loc[2,3] + u_loc[2,2]
+        u0N = -Δ.y*bcv0_loc[2,3] + u0_loc[2,2]
+    elseif type_loc[2,3] === :periodic || type_loc[2,3] === :in
+        uN = u_loc[2,3]
+        u0N = u0_loc[2,3]
+    end
+
+
+    # Heat flux for each face based on finite differences
+    qxW = -k.xx[1]*(uC - uW)/Δ.x  # West
+    qxE = -k.xx[2]*(uE - uC)/Δ.x  # East
+    qyS = -k.yy[1]*(uC - uS)/Δ.y  # South
+    qyN = -k.yy[2]*(uN - uC)/Δ.y  # North
+
+    # Heat flux of the previous time step for each face based on finite differences
+    qxW0 = -k.xx[1]*(u0C - u0W)/Δ.x  # West
+    qxE0 = -k.xx[2]*(u0E - u0C)/Δ.x  # East
+    qyS0 = -k.yy[1]*(u0C - u0S)/Δ.y  # South
+    qyN0 = -k.yy[2]*(u0N - u0C)/Δ.y  # North
+
+    return -( θ * (-(qxE - qxW)/Δ.x - (qyN - qyS)/Δ.y) + (1-θ) * (-(qxE0 - qxW0)/Δ.x - (qyN0 - qyS0)/Δ.y) - ρ*cp*(uC - u0C)/Δ.t + s)
+end
+
 # This function loop over the whole set of 2D cells and computes the residual in each cells
-function ResidualPoisson2D!(R, u, k, s, num, type, bc_val, nc, Δ, u0, ρ, cp)  # u_loc, s, type_loc, Δ
+function ResidualDiffusion2D!(R, u, k, s, num, type, bc_val, bc_val0, nc, Δ, u0, ρ, cp, θ, Scheme)
 
     # This is just a vector of zeros (off-diagonal terms of the conductivity tensor are 0 in the isotropic case)
     # Here StaticArrays are being used a bit everywhere. This is a Julia Library which is used for achieving good performance.
@@ -95,8 +165,9 @@ function ResidualPoisson2D!(R, u, k, s, num, type, bc_val, nc, Δ, u0, ρ, cp)  
     # Loop over the cells in 2D
     for j in 1+shift.y:nc.y+shift.y, i in 1+shift.x:nc.x+shift.x
 
-        # This is the 3*3 matrix that contains the current stencil values (see residual function above) 
+        # 3x3 matrices that contain the current and previous stencil values, respectively
         u_loc     =  SMatrix{3,3}(u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
+        u0_loc    =  SMatrix{3,3}(u0[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
 
         # Conductivity is also stored into Static arrays - can be simplified
         k_loc_xx  = @SVector [k.x.xx[i-1,j-1], k.x.xx[i,j-1]] # W and E values
@@ -106,12 +177,16 @@ function ResidualPoisson2D!(R, u, k, s, num, type, bc_val, nc, Δ, u0, ρ, cp)  
 
         # This stores the VALUE of the boundary condition for the stencils (3*3 matrix)
         bcv_loc   = SMatrix{3,3}(bc_val.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
+        bcv0_loc  = SMatrix{3,3}(bc_val0.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
 
         # This stores the TYPE of the boundary condition for the stencils (3*3 matrix)
         type_loc  = SMatrix{3,3}(  type.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
 
-        # This calls the residual function
-        R[i,j]    = Diffusion2D(u_loc, k_loc, s[i,j], type_loc, bcv_loc, Δ, u0[i,j], ρ[i,j], cp[i,j])
+        if Scheme ==:CrankNicolson
+            R[i,j]    = Diffusion2D_CrankNicolson(u_loc, u0_loc, k_loc, s[i,j], type_loc, bcv_loc, bcv0_loc, Δ, ρ[i,j], cp[i,j], θ)
+        elseif Scheme ==:BackwardEuler
+            R[i,j]    = Diffusion2D_backwardEuler(u_loc, k_loc, s[i,j], type_loc, bcv_loc, Δ, u0[i,j], ρ[i,j], cp[i,j])
+        end
     end
     return nothing
 end
@@ -121,7 +196,7 @@ end
 # The other does it with Enzyme: https://github.com/EnzymeAD/Enzyme.jl
 
 # Computation of the partial derivatives of the residual function. Sets the coefficient in the system matrix.
-function AssemblyPoisson_Enzyme!(K, u, k, s, number, type, pattern, bc_val, nc, Δ, u0, ρ, cp)
+function AssemblyDiffusion_Enzyme!(K, u, k, s, number, type, pattern, bc_val, bc_val0, nc, Δ, u0, ρ, cp, θ, Scheme)
 
     # This is aocal matrix that stores the partial derivatives.
      
@@ -145,8 +220,9 @@ function AssemblyPoisson_Enzyme!(K, u, k, s, number, type, pattern, bc_val, nc, 
         # This is a 3*3 matrix that contains the number of each equation in the stencil (each cell has one corresponding value of u, which has a unique equation number)
         num_loc   = SMatrix{3,3}(number.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1) .* pattern.u.u
 
-        # This is the 3*3 matrix that contains the current stencil values (see residual function above) 
+        # T3*3 matrices containing the current and previous stencil values
         u_loc     = MMatrix{3,3}(u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
+        u0_loc    = MMatrix{3,3}(u0[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
 
         # Same as above: conductivity tensor
         k_loc_xx  = @SVector [k.x.xx[i-1,j-1], k.x.xx[i,j-1]]
@@ -156,6 +232,8 @@ function AssemblyPoisson_Enzyme!(K, u, k, s, number, type, pattern, bc_val, nc, 
 
         # Same as above: boundary condition VALUES
         bcv_loc   = SMatrix{3,3}(bc_val.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
+        bcv0_loc  = SMatrix{3,3}(bc_val0.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
+
         # Same as above: boundary condition TYPES
         type_loc  = SMatrix{3,3}(  type.u[ii,jj] for ii in i-1:i+1, jj in j-1:j+1)
 
@@ -163,7 +241,11 @@ function AssemblyPoisson_Enzyme!(K, u, k, s, number, type, pattern, bc_val, nc, 
         ∂R∂u     .= 0e0
 
         # Here the magic happens: we call a function from Enzyme that computes all, partial derivatives for the current stencil block 
-        autodiff(Enzyme.Reverse, Diffusion2D, Duplicated(u_loc, ∂R∂u), Const(k_loc), Const(s[i,j]), Const(type_loc), Const(bcv_loc), Const(Δ), Const(u0[i,j]), Const(ρ[i,j]), Const(cp[i,j]))
+        if Scheme ==:CrankNicolson
+            autodiff(Enzyme.Reverse, Diffusion2D_CrankNicolson, Duplicated(u_loc, ∂R∂u), Const(u0_loc), Const(k_loc), Const(s[i,j]), Const(type_loc), Const(bcv_loc), Const(bcv0_loc), Const(Δ), Const(ρ[i,j]), Const(cp[i,j]), Const(θ))
+        elseif Scheme ==:BackwardEuler
+            autodiff(Enzyme.Reverse, Diffusion2D_backwardEuler, Duplicated(u_loc, ∂R∂u), Const(k_loc), Const(s[i,j]), Const(type_loc), Const(bcv_loc), Const(Δ), Const(u0[i,j]), Const(ρ[i,j]), Const(cp[i,j]))
+        end
 
         # This loops through the 2*2 stencil block and sets the coefficient ∂R∂u into the sparse matrix K.u.u
         num_ij = number.u[i,j]
@@ -188,7 +270,7 @@ function AnalyticalDiffusion2D(u_ana, x, y, t, nc, params)
     end
 end
 
-function BC_Analytical(bc_val, xc, yc, t, L, params)
+function BC_Analytical2D(bc_val, xc, yc, t, L, params)
     for j in axes(bc_val.u, 2)
         # West
         X      = @SVector[-L/2; yc[j]; t]
@@ -211,7 +293,7 @@ function BC_Analytical(bc_val, xc, yc, t, L, params)
     end
 end
 
-function RunDiffusion(nc, L, Δt0) 
+function RunDiffusion(nc, L, Δt0, Scheme) 
 
     # This is the main code that calls the above functions!
     to = TimerOutput()
@@ -233,6 +315,7 @@ function RunDiffusion(nc, L, Δt0)
 
     # Define values of the boundary conditions
     bc_val = Fields( fill(0., (nc.x+2, nc.y+2)) )   # Achtung: geist nodes
+    bc_val0 = Fields( fill(0., (nc.x+2, nc.y+2)) )
     bc_val.u        .= zeros(nc.x+2, nc.y+2)        # useless ?!                         # North
 
     # 5-point stencil, this is the definition of the stencil block. 
@@ -255,6 +338,7 @@ function RunDiffusion(nc, L, Δt0)
     # nout       = 20
     nt         = Int64(ceil(total_time/Δt0))
     t          = 0.
+    θ          = 0.5   # Crank-Nicolson parameter
 
     # Arrays
     # div( q ) - s = 0
@@ -306,14 +390,15 @@ function RunDiffusion(nc, L, Δt0)
         u0 .= u  # store the previous solution
 
         # Update BC
-        BC_Analytical(bc_val, xc, yc, t, L, params)
+        BC_Analytical2D(bc_val, xc, yc, t, L, params)
+        BC_Analytical2D(bc_val0, xc, yc, t-Δ.t, L, params)
 
         # Residual check: div( q ) - s = r
-        @timeit to "Residual" ResidualPoisson2D!(r, u, k, s, number, type, bc_val, nc, Δ, u0, ρ, cp) 
+        @timeit to "Residual" ResidualDiffusion2D!(r, u, k, s, number, type, bc_val, bc_val0, nc, Δ, u0, ρ, cp, θ, Scheme) 
         @info norm(r)/sqrt(length(r))
         
         @timeit to "Assembly Enzyme" begin
-            AssemblyPoisson_Enzyme!(M, u, k, s, number, type, pattern, bc_val, nc, Δ, u0, ρ, cp)
+            AssemblyDiffusion_Enzyme!(M, u, k, s, number, type, pattern, bc_val, bc_val0, nc, Δ, u0, ρ, cp, θ, Scheme)
         end
 
         @info "Symmetry"
@@ -327,7 +412,7 @@ function RunDiffusion(nc, L, Δt0)
         u[inx,iny] .-= reshape(du, nc...)   # update the solution u using the correction du
         
         # Residual check
-        ResidualPoisson2D!(r, u, k, s, number, type, bc_val, nc, Δ, u0, ρ, cp)     
+        ResidualDiffusion2D!(r, u, k, s, number, type, bc_val, bc_val0, nc, Δ, u0, ρ, cp, θ, Scheme)
         @info norm(r)/sqrt(length(r))
 
         # Analytic solution
@@ -347,6 +432,7 @@ let
     L   = 1.      # Domain extent
     nc0 = 30
     n   =  [1, 2, 4]
+    Scheme = :CrankNicolson # :BackwardEuler, :CrankNicolson
 
     Δx  = L ./ (n*nc0)
     Δt  = 0.01 ./ n
@@ -355,7 +441,7 @@ let
     # Calculate the error for each resolution
     for i in eachindex(n)
         nc = (x = n[i]*nc0, y = n[i]*nc0)
-        ϵu[i] = RunDiffusion(nc, L, Δt[i])
+        ϵu[i] = RunDiffusion(nc, L, Δt[i], Scheme)
     end
 
     # Visualization
